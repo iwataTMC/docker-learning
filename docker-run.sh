20d401f61fbf904dd696e50ad3343aab4cbaab81
iwataTMC <abcwwwxyz888@gmail.com>
2024-05-08 13:14:33 +0900

    Update

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 docker run -ti --rm --name network -p 8888:8888 network:latest /bin/bash
-# docker -v /Users/<host user>/<user-dir>:/home/<docker user>/<docker-user-dir> -ti --rm --name network -p 8888:8888 network:latest /bin/bash
+# docker run -v /Users/<host user>/<host-dir-name>:/home/<docker user>/<docker-dir-name> -ti --rm --name network -p 8888:8888 network:latest /bin/bash
